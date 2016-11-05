7c100dac2901f4d65b5bd7c190b454d7a23cdbc1
Connor Linfoot <connorlinfoot@me.com>
2016-11-05 15:45:04 +0000

    Added recipe for teleporter pad

--- src/main/java/org/devathon/contest2016/DevathonPlugin.java
+++ src/main/java/org/devathon/contest2016/DevathonPlugin.java
@@ -1,17 +1,32 @@
 package org.devathon.contest2016;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DevathonPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // put your enable code here
+        getLogger().info("Enabling Devathon Teleporter Plugin...");
+
+		// Custom recipe
+
+		ItemStack teleporterPad = new ItemStack(Material.IRON_PLATE);
+		ItemMeta teleporterPadMeta = teleporterPad.getItemMeta();
+		teleporterPadMeta.setDisplayName(ChatColor.GOLD + "Teleporter Pad");
+		teleporterPad.setItemMeta(teleporterPadMeta);
+
+		ShapedRecipe teleporterRecipe = new ShapedRecipe(teleporterPad).shape("rdr", "did", "rdr").setIngredient('r', Material.REDSTONE).setIngredient('d', Material.DIAMOND).setIngredient('i', Material.IRON_PLATE);
+		getServer().addRecipe(teleporterRecipe);
     }
 
     @Override
     public void onDisable() {
-        // put your disable code here
+        getLogger().info("Disabling Devathon Teleporter Plugin...");
     }
 }
 
